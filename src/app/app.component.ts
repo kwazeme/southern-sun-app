3f321b9547f1dd2d0f67827a572d5072e140059b
kwazeme <97014623+kwazeme@users.noreply.github.com>
2022-11-19 22:07:27 +0200

    initial commit 191122 2207

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'southern-sun-app';
+  title = 'Hotels Booking Frontpage App';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { GardenCourtComponent } from './garden-court/garden-court.component';
+import { ResortsComponent } from './resorts/resorts.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HotelsComponent,
+    GardenCourtComponent,
+    ResortsComponent,
   ],
   imports: [
     BrowserModule,
