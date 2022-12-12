9522ac68aefca6d5a437beab84e38d365c7fc232
kwazeme <97014623+kwazeme@users.noreply.github.com>
2022-12-12 22:04:59 +0200

    fixed preflightContinue CORS block. CRUD hotels=ok

--- server/app.js
+++ server/app.js
@@ -21,8 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers",
-  "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  res.setHeader("Access-Control-Allow-Headers","*");
+  // "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
   res.setHeader("Access-Control-Allow-Methods",
   "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   next();
