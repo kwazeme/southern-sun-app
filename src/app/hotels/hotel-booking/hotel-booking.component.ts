bf1956d537595da88ebd83045b6e34a00e59c4ee
kwazeme <97014623+kwazeme@users.noreply.github.com>
2022-12-17 21:49:54 +0200

    bookingModal populate clicked hotelbyId

--- src/app/hotels/hotel-booking/hotel-booking.component.ts
+++ src/app/hotels/hotel-booking/hotel-booking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
 import { Subscription } from 'rxjs';
 import { Hotel } from '../hotels.model';
 import { HotelsService } from '../hotels.service';
@@ -9,20 +11,33 @@ import { HotelsService } from '../hotels.service';
   styleUrls: ['./hotel-booking.component.css']
 })
 export class HotelBookingComponent implements OnInit {
-  hotels: Hotel[] = [];
-  subscription: Subscription;
+  hotel: Hotel;
   id: string;
+  subscription: Subscription;
 
   constructor(
     private hotelsService: HotelsService,
+    private route: Router,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.hotelsService.getHotelbyId(this.id);
-    this.subscription = this.hotelsService.getHotelUpdateListener()
-      .subscribe((hotels: Hotel[]) => {
-        this.hotels = hotels;
-      });
+    this.activatedRoute.params
+      .subscribe(
+        (params: Params) => {
+          this.id = params['id'];
+          this.hotel = this.hotelsService.getHotelbyId(params['id']);
+        }
+      )
   }
+
+  onClose() {
+    // this.route.navigate(['hotels']);
+    this.route.navigate([{ outlets: { hotelBookingModal: null }}]);
+  }
+
+  // ngOnDestroy(): void {
+  //   this.subscription.unsubscribe();
+  // }
 
 }
